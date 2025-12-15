3b32f9d7770f045398dc27082d35a21abc4b1539
Jacob Rosario <jacobrosario0225@gmail.com>
2025-12-14 19:07:33 -0800

    Halfway through Chapter 3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+// importing inter from google
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+// importing Lusitana font from google
+
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'],
+ });
